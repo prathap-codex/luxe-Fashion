d665e2eddce4e4382148d0af4554d8867287e6da
prathap-codex <prathapkrishnaveni12@gmail.com>
2026-08-15 20:36:37 +0530

    luxe fassion html and css completed

---
+++ index.js
@@ -0,0 +1,9 @@
+// ----------- top annoncement bar close ----------------
+const topBar = document.getElementById("topBar");
+const closeTopBar = document.getElementById("closeTopBar");
+
+closeTopBar.addEventListener("click", () => {
+    topBar.classList.add("hidden");
+});
+
+// ------------- Mobile menu ----------------
